99d090ad10fdbb903d1db9720199c76f127b297a
Alexandru Pupsa <Alexandru.Pupsa@gpn-telecom.net>
2024-06-12 13:54:49 +0300

    Update

--- FetchCSVfromBEC/Fetcher.cs
+++ FetchCSVfromBEC/Fetcher.cs
@@ -16,7 +16,7 @@ namespace FetchCSVfromBEC
         }
 
         [Function("fetch-csv")]
-        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
             var response = req.CreateResponse();
 
